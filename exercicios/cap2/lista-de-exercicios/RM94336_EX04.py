45c5ddcadb5567331478c939de2a17370ed50013
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-25 12:27:04 -0300

    Update RM94336_EX04.py

--- exercicios/cap2/lista-de-exercicios/RM94336_EX04.py
+++ exercicios/cap2/lista-de-exercicios/RM94336_EX04.py
@@ -3,3 +3,14 @@
 
 Ao analisar o código do programa deles, porém, você descobre que a senha é composta da palavra “LIBERDADE” seguida do fatorial dos minutos que a máquina estiver marcando no momento da digitação da senha (se a máquina estiver marcando 5 minutos, a senha será LIBERDADE120). Crie um programa que receba do usuário os minutos atuais e exiba na tela a senha necessária para desbloqueio. ATENÇÃO: seu programa não pode utilizar funções prontas para o cálculo do fatorial. Ele deve obrigatoriamente utilizar loop.
 """
+
+print('\nPrograma para gerar de desbloqueio do servidor do ataque Hacker!!!\n')
+print('Descobrimos que a senha é a palavra LIBERDADE + o calculo de fatorial dos minutos no seu computador.\n')
+minuto = input('Digite os minutos que aparecem neste computador: ')
+minuto = int(minuto)
+
+fatorial = 1
+for i in range (minuto, 0, -1):
+    fatorial *= i
+
+print(f'\nA senha que você precisa digitar é LIBERDADE{fatorial} para desbloquear o servidor.\nAtenção!!!: você tem 60 segundos validos até que a senha mude novamente!!!\n')
