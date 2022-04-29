d249ceac656b6a222ec1d530498275cd2509bd92
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-28 21:48:37 -0300

    Update arquivo2.py

--- enterprise-connection/idwall/arquivo2.py
+++ enterprise-connection/idwall/arquivo2.py
@@ -1,5 +1,8 @@
 
-""" lista = [
+from textwrap import indent
+
+
+lista = [
     ['5/9/2020', 'Apple', 'senha', 'ajuda de senha', 'nome'],
     ['21/7/2011', 'Microsoft', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
     ['14/7/2014', 'Facebook', 'senha', 'ajuda de senha', 'telefone'],
@@ -8,7 +11,7 @@
     [ '3/2/2012', 'Shoppe', 'ajuda de senha'],
     ['29/10/2006', 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome'],
     ['17/3/2000', 'Tim', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
-] """
+]
 
 """ lista = [
     ['21/7/2011', 'Microsoft', 'senha'],
@@ -25,14 +28,14 @@
     ['17/3/2000', 'Band', 'senha', 'ajuda de senha','telefone', 'nome', 'email'],
 ] """
 
-lista = [
+""" lista = [
     ['21/7/2011', 'Microsoft', 'ajuda de senha', 'telefone', 'nome', 'email'],
     ['29/10/2006', 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome'],
     ['29/12/2010', 'Shoppe', 'ajuda de senha'],
     ['29/10/2006', 'silvester', 'ajuda de senha', 'telefone'],
     ['29/12/2010', 'Amazon', 'senha', 'telefone'],
     ['21/7/2011', 'Tecsul', 'senha'],
-]
+] """
 
 tipos_de_dados = ['senha', 'ajuda de senha','telefone', 'nome', 'email']
 
@@ -48,14 +51,19 @@ for l, k in enumerate(tipos_de_dados):
             j = 0
 
         while j < len(lista):
+            for m in lista:
+                print(lista)
+            
             if i == len(lista) - 1:
                 break 
 
             if lista[j][2] == k and len(lista[j]) - 2 == contador - l:
                 lista[j], lista[i] = lista[i], lista[j]
                 i += 1
+                
             else:
                 j += 1
+                
                 
         contador -= 1 
     
