40caa39b1a07849d9754d97afee2d597cac29d78
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-29 18:58:30 -0300

    Update arquivo2.py

--- enterprise-connection/idwall/arquivo2.py
+++ enterprise-connection/idwall/arquivo2.py
@@ -1,32 +1,5 @@
 
 
-""" lista = [
-    ['5/9/2020', 'Apple', 'senha', 'ajuda de senha', 'nome'],
-    ['21/7/2011', 'Microsoft', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
-    ['14/7/2014', 'Facebook', 'senha', 'ajuda de senha', 'telefone'],
-    ['27/4/2006', 'Google', 'senha'],
-    ['29/12/2010', 'Amazon', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
-    [ '3/2/2012', 'Shoppe', 'ajuda de senha'],
-    ['29/10/2006', 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome'],
-    ['17/3/2000', 'Tim', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
-] """
-
-""" lista = [
-    ['21/7/2011', 'Microsoft', 'senha'],
-    ['29/10/2006', 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome'],
-    ['29/12/2010', 'Amazon', 'senha', 'telefone'],
-    ['21/7/2011', 'Tecsul', 'senha'],
-    ['14/7/2014', 'Facebook', 'senha', 'ajuda de senha', 'telefone'],
-    ['17/3/2000', 'Tim', 'senha', 'ajuda de senha','telefone', 'nome', 'email'],
-    ['29/10/2006', 'Vivo', 'senha', 'ajuda de senha', 'telefone', 'nome'],
-    ['17/3/2000', 'Claro', 'senha', 'ajuda de senha','telefone', 'nome', 'email'],
-    ['29/10/2006', 'Orkut', 'senha', 'ajuda de senha', 'telefone', 'nome'],
-    ['14/7/2014', 'Twiter', 'senha', 'ajuda de senha', 'telefone'],
-    ['21/7/2011', 'CNN', 'senha'],
-    ['17/3/2000', 'Band', 'senha', 'ajuda de senha','telefone', 'nome', 'email'],
-] """
-
-
 lista = [
     [[2022, 7, 10], 'Microsoft', 'ajuda de senha', 'telefone', 'nome', 'email'],
     [[2021, 6, 11], 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome'],
@@ -37,46 +10,42 @@ lista = [
     [[2011, 2, 15], 'Tecsul', 'senha'],
 ]
 
-tipos_de_dados = ['senha', 'ajuda de senha', 'telefone', 'nome', 'email']
+lista_prioridade = ['senha', 'ajuda de senha', 'telefone', 'nome', 'email']
 
-i = 0
-j = 0
-
+linha = 0
 ordem = []
-for l, k in enumerate(tipos_de_dados):
+for indice, valor_prioridade in enumerate(lista_prioridade):
     
-    contador = len(tipos_de_dados)
+    contador = len(lista_prioridade)
     while contador > 0:
         
-            if j >= len(lista):
-                    j = 0
+            if linha >= len(lista):
+                    linha = 0
 
             ordena_data = []
-            while j < len(lista):
+            while linha < len(lista):
                 
-                if lista[j][2] == k and len(lista[j]) - 2 == contador - l:
-                    d = lista[j]
-                    ordena_data.append(d)
+                if lista[linha][2] == valor_prioridade and len(lista[linha]) - 2 == contador - indice:
+
+                    ordena_data.append(lista[linha])
                     ordena_data.sort(reverse=True)
-                    j += 1
+                    linha += 1
+                    
                 else:
-                    j += 1
+                    linha += 1
                     
             contador -= 1
             
-            for r in ordena_data:
-                if len(ordena_data) == 0 or len(ordena_data) == 1:
-                    ordem.append(r)
-                else:
-                    if r[-1][0][1] > r[0][0][1]:
-                        r[-1], r[0] = r[0], r[-1]
-                        ordem.append(r)
+            for valor_linha in ordena_data:
+                ordem.append(valor_linha)
                     
 
-for j in lista:
-    print(j)
+for i in lista:
+    print(i)
+
 print()
-for c in ordem:
-    print(c)
+
+for j in ordem:
+    print(j)
 
 print(len(lista) == len(ordem))
