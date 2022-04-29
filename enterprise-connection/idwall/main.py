9991b89524812f91c279ad6e05534f62cd761c9a
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-29 19:09:09 -0300

    Update main.py

--- enterprise-connection/idwall/main.py
+++ enterprise-connection/idwall/main.py
@@ -1,47 +1,57 @@
 from gerador_data_e_dados import gerar_data, gerar_dados_classificados, definir_tipo_de_dados
 
 
-
 empresas = ['Apple', 'Microsoft', 'Facebook', 'Google', 'Amazon', 'Shoppe', 'Pentagono', 'Tim']
-tipos_de_dados = definir_tipo_de_dados()
-
 
 lista = []
 for empresa in empresas:
-    a = gerar_data()
+    a = list(gerar_data())
+    a[0], a[2] = a[2], a[0]
     b = gerar_dados_classificados()
-    variavel = [empresa, a]
+    variavel = [a, empresa]
     variavel.extend(b)
     lista.append(variavel)
 
 #### Algoritmo para ordenar pela criticidade dos dados
-i = 0
-j = 0
+lista_prioridade = definir_tipo_de_dados()
 
-for l, k in enumerate(tipos_de_dados):
+linha = 0
+ordem = []
+for indice, valor_prioridade in enumerate(lista_prioridade):
     
-    contador = len(tipos_de_dados)
+    contador = len(lista_prioridade)
     while contador > 0:
         
-        if j >= len(lista):
-            j = 0
+            if linha >= len(lista):
+                    linha = 0
 
-        while j < len(lista):
+            ordena_data = []
+            while linha < len(lista):
+                
+                if lista[linha][2] == valor_prioridade and len(lista[linha]) - 2 == contador - indice:
 
-            if i == len(lista) - 1:
-                break 
+                    ordena_data.append(lista[linha])
+                    ordena_data.sort(reverse=True)
+                    linha += 1
+                    
+                else:
+                    linha += 1
+                    
+            contador -= 1
+            
+            for valor_linha in ordena_data:
+                ordem.append(valor_linha)
+                    
 
-            if lista[j][2] == k and len(lista[j]) - 2 == contador - l:
-                lista[j], lista[i] = lista[i], lista[j]
-                i += 1
-                j += 1
-            else:
-                j += 1
-                 
-        contador -= 1 
-    
-for j in lista:
+for i in lista:
+    print(i)
+
+print()
+
+for j in ordem:
     print(j)
+
+print(len(lista) == len(ordem))
 
 
 
