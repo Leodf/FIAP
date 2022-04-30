063977c48dc52d9b220e319e4bca83df590b91d6
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-29 22:01:05 -0300

    commit

--- enterprise-connection/idwall/algoritmo.py
+++ enterprise-connection/idwall/algoritmo.py
@@ -1,13 +1,22 @@
 
 
 lista = [
-    [[2022, 7, 10], 'Microsoft', 'ajuda de senha', 'telefone', 'nome', 'email'],
-    [[2021, 6, 11], 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome'],
-    [[2011, 5, 12], 'Shoppe', 'ajuda de senha', 'telefone'],
-    [[2011, 6, 12], 'Claro', 'ajuda de senha', 'email'],
-    [[2011, 4, 13], 'Vivo', 'ajuda de senha', 'nome'],
-    [[1995, 3, 14], 'Amazon', 'senha', 'telefone'],
-    [[2011, 2, 15], 'Tecsul', 'senha'],
+    [[1997, 3, 5], 'Apple', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
+    [[2017, 3, 7], 'Microsoft', 'ajuda de senha', 'telefone', 'nome', 'email'],
+    [[1999, 4, 13], 'Facebook', 'senha', 'telefone'],
+    [[2019, 7, 11], 'Google', 'ajuda de senha', 'nome'],
+    [[2000, 2, 25], 'Amazon', 'nome'],
+    [[1993, 3, 22], 'Shoppe', 'ajuda de senha', 'nome'],
+    [[1996, 1, 2], 'Pentagono', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
+    [[1991, 2, 11], 'Tim', 'telefone'],
+    [[2002, 6, 13], 'Tecsul', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
+    [[2009, 6, 18], 'SKA', 'senha', 'ajuda de senha', 'telefone', 'email'],
+    [[2014, 3, 28], 'Tesla', 'senha', 'ajuda de senha', 'nome'],
+    [[2017, 1, 11], 'SpaceX', 'telefone', 'nome', 'email'],
+    [[2012, 8, 23], 'China', 'ajuda de senha'],
+    [[2004, 5, 11], 'Orkut', 'senha', 'ajuda de senha', 'telefone', 'nome', 'email'],
+    [[2013, 7, 14], 'Tio da padaria', 'senha'],
+    [[2004, 8, 16], 'ze da esquina', 'senha', 'ajuda de senha', 'nome', 'email'],
 ]
 
 lista_prioridade = ['senha', 'ajuda de senha', 'telefone', 'nome', 'email']
@@ -15,12 +24,12 @@ lista_prioridade = ['senha', 'ajuda de senha', 'telefone', 'nome', 'email']
 linha = 0
 ordem = []
 for indice, valor_prioridade in enumerate(lista_prioridade):
-    
+
     contador = len(lista_prioridade)
     while contador > 0:
         
             if linha >= len(lista):
-                    linha = 0
+                linha = 0
 
             ordena_data = []
             while linha < len(lista):
--- enterprise-connection/idwall/main.py
+++ enterprise-connection/idwall/main.py
@@ -1,7 +1,7 @@
 from gerador_data_e_dados import gerar_data, gerar_dados_classificados, definir_tipo_de_dados
 
 
-empresas = ['Apple', 'Microsoft', 'Facebook', 'Google', 'Amazon', 'Shoppe', 'Pentagono', 'Tim']
+empresas = ['Apple', 'Microsoft', 'Facebook', 'Google', 'Amazon', 'Shoppe', 'Pentagono', 'Tim', 'Tecsul', 'SKA', 'Tesla', 'SpaceX', 'China', 'Orkut', 'Tio da padaria', 'ze da esquina']
 
 lista = []
 for empresa in empresas:
